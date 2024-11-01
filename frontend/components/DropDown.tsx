2925c8389756a36b7e83130b36ba62e531dd07e4
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-11-02 02:29:37 +0530

    fix:styled the drop down component

--- frontend/components/DropDown.tsx
+++ frontend/components/DropDown.tsx
@@ -6,10 +6,10 @@ interface DropDownProps {
 export default function DropDown({ title, options }: DropDownProps) {
     return (
         <div className="">
-            <select className="p-2 bg-white  cursor-pointer" name={title}>
+            <select className="border border-gray-200 p-2 rounded-md  bg-white  cursor-pointer " name={title}>
                 {options.map((option, i) => {
                     return (
-                        <option className="bg-white p-2" key={i} value={option} > {option}</option>
+                        <option className="bg-gray-700 text-white" key={i} value={option} > {option}</option>
                     )
                 })}
             </select>
